03f4032db082875a9e5d23d2821d4826c30c66b7
TS Hsu <ts.hsu@mediatek.com>
2023-09-10 15:45:06 +0800

    [ALPS08258165] Modify RNR handle flow
    If RNR channel is 6G, not to ignore it even the channel
    already exist in current scan channel list. Because we
    might need to send probe request with BSSID to find 6G AP.
    
    MTK-Commit-Id: a3b3e4b16d853b37ca5ab354d53f389526f425fb
    
    Change-Id: Ic69a20b95bfffb2a966ce0a6ef695dd4e6d19253
    CR-Id: ALPS08258165
    Feature: [Module]Wi-Fi Driver
    Signed-off-by: TS Hsu <ts.hsu@mediatek.com>
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/7958978
    Test: srv_pf_nep_sanity
    Build: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Build: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    AutoUT-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Test: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    Reviewed-by: sticky.chen <sticky.chen@mediatek.com>
    Build: srv_preflight_b001 <srv_preflight_b001@mediatek.com>
    Commit-Check: srv_check_service <srv_check_service@mediatek.com>
    Coverity-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>

--- mgmt/scan.c
+++ mgmt/scan.c
@@ -1756,7 +1756,9 @@ uint8_t scanRnrChnlIsNeedScan(struct ADAPTER *prAdapter,
 			}
 		}
 	} else if (prScanParam->eScanChannel == SCAN_CHANNEL_FULL) {
-		if (fgIsLegalCh) {
+#if (CFG_SUPPORT_WIFI_6G == 1)
+		if (eRfBand != BAND_6G) {
+#endif
 			log_dbg(SCN, INFO,
 					"[ch:%d][band:%d] already in scan chnl list\n",
 						ucRnrChNum, eRfBand);
