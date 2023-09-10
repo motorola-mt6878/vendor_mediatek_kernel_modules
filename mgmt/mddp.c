9310a6ecc3b9a56a1ac332c2f5e48e078112674d
Jo-Ping.Li <Jo-Ping.li@mediatek.com>
2023-09-10 15:45:20 +0800

    [ALPS08329649] add flow to avoid set pcie aspm before pcie probe done
    This reverts commit d02db85eb70c ("[ALPS08329649] add
    flow to avoid set pcie aspm before pcie probe done")
    workaround solution removed due to sdes still occurred.
    
    MTK-Commit-Id: 924fb82e26069377d4b72981ae0816c9ad125f65
    
    CR-Id: ALPS08329649
    Feature: Wi-Fi Driver CONNAC
    Signed-off-by: jo-ping.li <jo-ping.li@mediatek.com>
    Change-Id: Ibe477bece90cb1de44a1b781c0fd83b84c5d4608
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/7978545
    Reviewed-by: holiday.hao <holiday.hao@mediatek.com>
    Build: srv_preflight_b001 <srv_preflight_b001@mediatek.com>
    Reviewed-by: austin.lai <austin.lai@mediatek.com>
    Reviewed-by: ilex.lin <ilex.lin@mediatek.com>
    Test: srv_pf_nep_sanity
    Build: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Coverity-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Commit-Check: srv_check_service <srv_check_service@mediatek.com>
    Build: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    AutoUT-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Test: srv_preflight_a001 <srv_preflight_a001@mediatek.com>

--- mgmt/mddp.c
+++ mgmt/mddp.c
@@ -1611,15 +1611,13 @@ int32_t mddpMdNotifyInfo(struct mddpw_md_notify_info_t *prMdInfo)
 #if CFG_SUPPORT_PCIE_ASPM
 		} else if (event->u4Reason == MD_L12_DISABLE) {
 			/* disable PCIe L1.2, 2 means md config */
-			if (mtk_get_pci_probe_status() &&
-				prBusInfo->configPcieAspm) {
+			if (prBusInfo->configPcieAspm) {
 				prBusInfo->configPcieAspm(prGlueInfo, FALSE, 2);
 				mddpNotifyMDPCIeL12Status(0);
 			}
 		} else if (event->u4Reason == MD_L12_ENABLE) {
 			/* enable PCIe L1.2, 2 means md config */
-			if (mtk_get_pci_probe_status() &&
-				prBusInfo->configPcieAspm) {
+			if (prBusInfo->configPcieAspm) {
 				prBusInfo->configPcieAspm(prGlueInfo, TRUE, 2);
 				mddpNotifyMDPCIeL12Status(1);
 			}
