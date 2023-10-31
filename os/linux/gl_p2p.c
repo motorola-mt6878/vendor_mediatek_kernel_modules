2548ba5ffec564f5762335f29ef6d4ee5797a101
Ben Lai <ben.lai@mediatek.com>
2023-10-31 10:00:55 +0800

    [ALPS08486530] sap: Remove updating mode message
    sap: Remove updating mode message.
    
    MTK-Commit-Id: b63d5f2f3095dcf7a65a07f3ec1c2930c214a46e
    
    CR-Id: ALPS08486530
    Signed-off-by: Ben Lai <ben.lai@mediatek.com>
    Feature: Wi-Fi Driver
    Change-Id: Ic595417be1b6454f699e7631cf5a52faeb69292a
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/8123158
    Commit-Check: srv_check_service <srv_check_service@mediatek.com>
    Build: srv_preflight_b001 <srv_preflight_b001@mediatek.com>
    Coverity-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Reviewed-by: chris.hong <chris.hong@mediatek.com>
    Build: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    AutoUT-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Test: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    Build: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    Test: srv_pf_nep_sanity

--- os/linux/gl_p2p.c
+++ os/linux/gl_p2p.c
@@ -1039,10 +1039,8 @@ int glSetupP2P(struct GLUE_INFO *prGlueInfo, struct wireless_dev *prP2pWdev,
 				(void *)prGlueInfo->prP2PInfo->prDevHandler);
 #endif
 
-	mtk_p2p_cfg80211_change_iface_impl(prAdapter, u4Idx,
-					   prP2pWdev->iftype);
-
 exit:
+
 	return 0;
 }
 
