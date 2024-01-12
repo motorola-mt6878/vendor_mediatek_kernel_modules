91d7a2671699cd19ad8597e469eda0076b0c55c6
Chris.Lu <chris.lu@mediatek.com>
2024-01-12 20:32:53 +0800

    [WCNCR00244155] fix sdio driver build failed on kernel v4.12
    [Description]
    1. build failed on kernel version 4.12. since wrong kernel version judgement.
       sched.h change to sched/types.h start from v4.11 rather than v4.14
    
    [Test Report]
    test on ubuntu pc(kernel v4.12) + boots -c reset/boots -c inq pass.
    
    MTK-Commit-Id: 91040ecaf89a4db7df234706c2ff4237fb8905aa
    
    CR-Id: WCNCR00244155
    Signed-off-by: Chris Lu <chris.lu@mediatek.com>
    Feature: BT_Driver
    Change-Id: I96200179551e2487b212027a5eabeca6ccd23cb4

--- sdio/btmtksdio.c
+++ sdio/btmtksdio.c
@@ -22,7 +22,7 @@
  */
 
 #include <linux/version.h>
-#if (KERNEL_VERSION(4, 14, 0) > LINUX_VERSION_CODE)
+#if (KERNEL_VERSION(4, 11, 0) > LINUX_VERSION_CODE)
 #include <linux/sched.h>
 #else
 #include <uapi/linux/sched/types.h>
