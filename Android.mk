cd704633716c1b66f824f7a809a44f73a34bf17f
Joe-CH.Tsai <Joe-CH.Tsai@mediatek.com>
2024-01-12 19:41:28 +0800

    [ALPS05684106] MET: revise met v3 driver build condition
    Build met v3 driver starting from kernel version 5.10
    
    MTK-Commit-Id: fe961b13bc034187b747ec8edaa47bdb22877045
    
    Change-Id: Ia23c33da09c3d8766fb31c0c87b1b1bb9bfeaf6e
    Signed-off-by: Joe-CH.Tsai <Joe-CH.Tsai@mediatek.com>
    CR-Id: ALPS05684106
    Feature: [Module]MET (Mediatek Embedded Technology)

--- Android.mk
+++ Android.mk
@@ -4,7 +4,7 @@ LOCAL_PATH := $(call my-dir)
 KERNEL_VERSION := $(word 2,$(subst -, ,$(LINUX_KERNEL_VERSION)))
 MAJOR_VERSION := $(shell echo $(KERNEL_VERSION) | cut -f1 -d.)
 MINOR_VERSION := $(shell echo $(KERNEL_VERSION) | cut -f2 -d.)
-VERSION_CHECK := $(shell test $(MAJOR_VERSION) -ge 5 || test $(MAJOR_VERSION) -ge 4 -a $(MINOR_VERSION) -ge 19 && echo true)
+VERSION_CHECK := $(shell test $(MAJOR_VERSION) -ge 5 -a $(MINOR_VERSION) -ge 10 && echo true)
 
 ifeq ($(VERSION_CHECK), true)
 
